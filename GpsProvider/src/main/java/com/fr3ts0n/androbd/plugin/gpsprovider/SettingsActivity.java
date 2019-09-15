b3e53afffd056af0115ca303ac7ba046c074fa15
fr3ts0n <fr3ts0n@gmx.at>
2019-09-15 14:13:45 +0200

    GpsProvider: Fix crash on android < M

--- GpsProvider/src/main/java/com/fr3ts0n/androbd/plugin/gpsprovider/SettingsActivity.java
+++ GpsProvider/src/main/java/com/fr3ts0n/androbd/plugin/gpsprovider/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.fr3ts0n.androbd.plugin.gpsprovider;
 
 import android.Manifest;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceActivity;
@@ -21,6 +22,11 @@ public class SettingsActivity extends PreferenceActivity
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            requestPermissions(new String[]{ Manifest.permission.ACCESS_COARSE_LOCATION,
+                                             Manifest.permission.ACCESS_COARSE_LOCATION },
+                               0);
+        }
     }
 }
