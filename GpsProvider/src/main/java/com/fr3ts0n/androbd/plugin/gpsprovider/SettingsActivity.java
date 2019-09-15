be63c6d2ee8d46162e257ad5ec858474bcd7bdda
fr3ts0n <fr3ts0n@gmx.at>
2019-09-15 14:15:21 +0200

    Optimize imports

--- GpsProvider/src/main/java/com/fr3ts0n/androbd/plugin/gpsprovider/SettingsActivity.java
+++ GpsProvider/src/main/java/com/fr3ts0n/androbd/plugin/gpsprovider/SettingsActivity.java
@@ -2,18 +2,9 @@ package com.fr3ts0n.androbd.plugin.gpsprovider;
 
 
 import android.Manifest;
-import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceActivity;
-import android.util.Log;
-
-import com.fr3ts0n.androbd.plugin.Plugin;
-
-import java.security.Permission;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class SettingsActivity extends PreferenceActivity
 {
